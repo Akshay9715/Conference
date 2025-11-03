e4ec56e1d18707903541163d58d6dca8aa36a28b
Akshay <akshaychoudhary2040@gmail.com>
2025-11-03 13:02:47 +0530

    Replace main with Hero-Section-Modify branch content

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -32,6 +32,7 @@ function HeroSection() {
         data-alt="A close-up high-resolution image of a 3D bioprinting scaffold."
       ></div> */}
       {/* Gradient Overlay */}
+
       <div
         className="absolute inset-0"
         style={{
@@ -42,8 +43,12 @@ function HeroSection() {
       {/* Hero Content */}
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-4">
         <div className="max-w-4xl">
-          <h1 className="text-3xl font-black leading-tight tracking-tight md:text-3xl">
-            Translation of Bioengineering Research for Healthcare and Society
+          <h1 className="text-4xl font-black leading-tight tracking-tight md:text-6xl uppercase">
+            National Bioengineering Conference 2025
+          </h1>
+          <h1 className="mt-6 text-2xl font-semibold md:text-3xl">
+            Theme: Translation of Bioengineering Research for Healthcare and
+            Society
           </h1>
           <h2 className="mt-4 text-lg font-medium md:text-xl">
             11<sup>th</sup>– 13<sup>th</sup>December 2025 NIT Rourkela
