a60cf470886cd4e349f6f69805153b19b0127426
Akshay <akshaychoudhary2040@gmail.com>
2025-11-10 23:20:02 +0530

    second partner added

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -3,9 +3,12 @@ import React from "react";
 const Partners = () => {
   const events = [
     {
-      icon: "partner1.png",
-      title: "Oral Presentation",
-      desc: "Featuring research findings and innovations.",
+      icon: "General/partner1.png",
+      desc: "The Sustainable Biomanufacturers and Research Society (SBRS) is delighted to partner with and endorse the Bioengineering Conference 2025. SBRS is also pleased to announce three poster awards for the upcoming poster competition. Additionally, we invite all aspiring and established biomanufacturers to join the dynamic and growing community.Register for SBRS membership by December 31, 2025, to avail the early-bird discount.",
+    },
+    {
+      icon: "General/ANRF.jpg",
+      desc: "The Anusandhan National Research Foundation (ANRF), India’s apex body for research and innovation under the ANRF Act, 2023, is pleased to partner with and support the Bioengineering Conference 2025. ANRF aims to strengthen research ecosystems and encourage collaboration among academia, industry, and government.",
     },
   ];
   return (
@@ -20,23 +23,12 @@ const Partners = () => {
             {events.map((event, index) => (
               <div
                 key={index}
-                className="flex flex-col items-center text-center gap-4 rounded-xl border-2 border-slate-200 dark:border-slate-800 bg-white p-5 hover:shadow-lg hover:-translate-y-1 transition-all duration-300 w-3xl"
+                className="flex flex-col mx-5 items-center text-center gap-4 rounded-xl border-2 border-slate-200 dark:border-slate-800 bg-white p-5 hover:shadow-lg hover:-translate-y-1 transition-all duration-300 w-3xl"
               >
                 <div className="flex-col items-center justify-center ">
-                  <img
-                    src="General/partner1.png"
-                    alt=""
-                    className="w-50 mx-auto"
-                  />
-                  <div className="text-text-light dark:text-text-dark/90 leading-relaxed max-w-3xl mx-auto text-justify">
-                    The Sustainable Biomanufacturers and Research Society (SBRS)
-                    is delighted to partner with and endorse the Bioengineering
-                    Conference 2025. SBRS is also pleased to announce three
-                    poster awards for the upcoming poster competition.
-                    Additionally, we invite all aspiring and established
-                    biomanufacturers to join the dynamic and growing community.
-                    Register for SBRS membership by December 31, 2025, to avail
-                    the early-bird discount.
+                  <img src={event.icon} alt="" className="w-50 mx-auto" />
+                  <div className="text-text-light dark:text-text-dark/90 leading-relaxed max-w-3xl mx-auto text-justify items-center">
+                    {event.desc}
                   </div>
                 </div>
               </div>
